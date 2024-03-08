393e12f9bab81534399801178aaeccc97c6c2871
Leo Bonato <bonato.leo@gmail.com>
2024-03-08 20:34:04 +0100

    better layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.pcss';
 </script>
 
-<slot />
+<div class="flex h-svh flex-col items-center overflow-hidden">
+	<div class="navbar gap-10 bg-base-100">
+		<a class="btn btn-ghost" href="#mission">Mission</a>
+		<a class="btn btn-ghost" href="#about">About</a>
+	</div>
+
+	<main class="flex w-full flex-col items-center overflow-scroll">
+		<slot />
+	</main>
+</div>
