8b2247fb9def91849b43a2df3f44b3ec3bad5938
Leo Bonato <bonato.leo@gmail.com>
2024-04-05 10:30:33 -0300

    extract about us

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,33 +7,7 @@
 	import fingerprint from '$lib/img/obfuscated-fingerprint-symmetrical.webp';
 	import future from '$lib/img/future.webp';
 	import padlock from '$lib/img/padlock.webp';
-	import portraitBonato from '$lib/img/portrait_bonato.jpeg';
-	import portraitDeCeglia from '$lib/img/portrait_de_ceglia.jpeg';
-	import portraitZamuner from '$lib/img/portrait_zamuner.jpeg';
-
-	const profiles = [
-		{
-			name: 'Leo Bonato',
-			degree: 'PhD Physics',
-			role: 'Co-founder',
-			img: portraitBonato,
-			linkedin: 'https://www.linkedin.com/in/leo-bonato/'
-		},
-		{
-			name: 'Roberta De Ceglia',
-			degree: 'PhD Translational Medicine',
-			role: 'Co-founder',
-			img: portraitDeCeglia,
-			linkedin: 'https://www.linkedin.com/in/robertadeceglia/'
-		},
-		{
-			name: 'Stefano Zamuner',
-			degree: 'PhD Physics',
-			role: 'Co-founder',
-			img: portraitZamuner,
-			linkedin: 'https://www.linkedin.com/in/stefano-zamuner/'
-		}
-	];
+	import AboutUs from './AboutUs.svelte';
 </script>
 
 <div class="flex flex-col items-center">
@@ -71,18 +45,5 @@
 		believe in protecting what's valuable — your privacy, your data, and your trust.
 	</HeroText>
 
-	<div class="prose mb-48 w-full max-w-3xl prose-a:text-secondary" id="about">
-		<h1 class="text-center">About us</h1>
-		<div class="flex flex-row justify-evenly">
-			{#each profiles as profile (profile.name)}
-				<div class="flex flex-col items-center">
-					<img src={profile.img} alt={profile.name} class="mb-4 h-48 w-48 rounded-full" />
-					<p class="m-0 text-lg font-bold">{profile.name}</p>
-					<p class="m-0 mb-2 text-sm">{profile.degree}</p>
-					<p class="m-0 mb-2">{profile.role}</p>
-					<a href={profile.linkedin}>LinkedIn</a>
-				</div>
-			{/each}
-		</div>
-	</div>
+	<AboutUs />
 </div>
---
+++ src/routes/AboutUs.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import portraitBonato from '$lib/img/portrait_bonato.jpeg';
+	import portraitDeCeglia from '$lib/img/portrait_de_ceglia.jpeg';
+	import portraitZamuner from '$lib/img/portrait_zamuner.jpeg';
+
+	const profiles = [
+		{
+			name: 'Leo Bonato',
+			degree: 'PhD Physics',
+			role: 'Co-founder',
+			img: portraitBonato,
+			linkedin: 'https://www.linkedin.com/in/leo-bonato/'
+		},
+		{
+			name: 'Roberta De Ceglia',
+			degree: 'PhD Translational Medicine',
+			role: 'Co-founder',
+			img: portraitDeCeglia,
+			linkedin: 'https://www.linkedin.com/in/robertadeceglia/'
+		},
+		{
+			name: 'Stefano Zamuner',
+			degree: 'PhD Physics',
+			role: 'Co-founder',
+			img: portraitZamuner,
+			linkedin: 'https://www.linkedin.com/in/stefano-zamuner/'
+		}
+	];
+</script>
+
+<div class="prose mb-48 w-full max-w-3xl prose-a:text-secondary" id="about">
+	<h1 class="text-center">About us</h1>
+	<div class="flex flex-row justify-evenly">
+		{#each profiles as profile (profile.name)}
+			<div class="flex flex-col items-center">
+				<img src={profile.img} alt={profile.name} class="mb-4 h-48 w-48 rounded-full" />
+				<p class="m-0 text-lg font-bold">{profile.name}</p>
+				<p class="m-0 mb-2 text-sm">{profile.degree}</p>
+				<p class="m-0 mb-2">{profile.role}</p>
+				<a href={profile.linkedin}>LinkedIn</a>
+			</div>
+		{/each}
+	</div>
+</div>
