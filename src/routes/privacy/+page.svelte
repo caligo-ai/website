04a1ca5fdc3e3ebfdc2c6ec0c5d13de818ddf3f1
Leo Bonato <bonato.leo@gmail.com>
2024-05-07 21:02:35 +0200

    add privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,7 +1,93 @@
-<div class="min-h-96 max-w-3xl px-20">
-	<p>We love privacy</p>
-	<p>Privacy is the best</p>
-	<p>Much private</p>
-	<p>So privacy</p>
-	<p>WOW</p>
+<script>
+	import PrivacyPolicyItem from './PrivacyPolicyItem.svelte';
+</script>
+
+<div class="prose  max-w-3xl px-20">
+	<h1>Privacy Policy</h1>
+
+	<ol>
+		<PrivacyPolicyItem title="Introduction">
+			Thank you for your interest in Caligos's privacy policy. We are committed to protecting your
+			privacy and ensuring that your personal data is protected when you use our website.
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Data Controller">
+			The data controller responsible for your personal information for the purposes of GDPR
+			compliance is:
+			<ul>
+				<li>Name: Caligo</li>
+				<li>Address: [Business Address]</li>
+				<li>Email: info@caligo.ai</li>
+			</ul>
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Information We Collect">
+			We collect the following types of information:
+			<ul>
+				<li>
+					Contact Information: Includes any information you provide when you contact us with
+					questions, feedback, or otherwise correspond with us online.
+				</li>
+			</ul>
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Purpose of Data Processing">
+			Your data is processed for the following purposes:
+			<ul>
+				<li>To respond to your inquiries and provide customer support.</li>
+				<li>To comply with legal obligations.</li>
+			</ul>
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Legal Basis for Processing">
+			The processing of your data is based on the following legal grounds:
+			<ul>
+				<li>Your consent, as per Article 6(1)(a) of the GDPR.</li>
+				<li>
+					Necessary for the purposes of legitimate interests pursued by us, as per Article 6(1)(f)
+					of the GDPR.
+				</li>
+			</ul>
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Data Retention">
+			We retain personal data only for as long as necessary to fulfill the purposes for which it was
+			collected, including for the purposes of satisfying any legal, accounting, or reporting
+			requirements.
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Data Sharing">
+			We do not share your personal data with third parties except as necessary to fulfill your
+			requests or comply with legal obligations.
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Your Rights">
+			Under GDPR and the Swiss FADP, you have the following rights:
+			<ul>
+				<li>To access and receive a copy of your personal data we hold about you.</li>
+				<li>To request correction of any incomplete or inaccurate data we hold about you.</li>
+				<li>
+					To request deletion of your personal data when it is no longer necessary for us to retain
+					such data.
+				</li>
+				<li>
+					To object to the processing of your data where we are relying on a legitimate interest.
+				</li>
+			</ul>
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Cookie Policy">
+			We do not use cookies.
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Changes to This Privacy Policy">
+			We may update this Privacy Policy from time to time. We will notify you of any changes by
+			posting the new Privacy Policy on this page.
+		</PrivacyPolicyItem>
+
+		<PrivacyPolicyItem title="Contact Us">
+			If you have any questions about this Privacy Policy, please contact us at the details provided
+			above.
+		</PrivacyPolicyItem>
+	</ol>
 </div>
---
+++ src/routes/privacy/PrivacyPolicyItem.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<li>
+	<h4>{title}</h4>
+	<p><slot /></p>
+</li>
