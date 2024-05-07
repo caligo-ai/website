5da741022ec0ac733dcde1c7f3c75f9120329ea1
Leo Bonato <bonato.leo@gmail.com>
2024-05-07 20:39:21 +0200

    change hero texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import Hero from './Hero.svelte';
 	import HeroText from './HeroText.svelte';
 
-	import algorithm from '$lib/img/algorithm.webp';
-	import bird from '$lib/img/bird.webp';
 	import fingerprint from '$lib/img/obfuscated-fingerprint-symmetrical.webp';
-	import future from '$lib/img/future.webp';
-	import padlock from '$lib/img/padlock.webp';
+	import privacy from '$lib/img/privacy.webp';
+	import processing from '$lib/img/processing.webp';
+	import mandala from '$lib/img/mandala.webp';
+	import isolated from '$lib/img/isolated.webp';
 	import AboutUs from './AboutUs.svelte';
 
-	import {base} from "$app/paths"
+	import { base } from '$app/paths';
 </script>
 
 <div class="flex flex-col items-center">
@@ -19,32 +19,32 @@
 		<a href="mailto:info@caligo.ai" class="btn-secondary-content btn">Get in touch</a>
 	</Hero>
 
-	<HeroText background={future} title="The Privacy Paradox in Today's AI">
-		Imagine a world where advanced AI can enhance our lives without compromising our privacy. Where
-		video surveillance ensures public safety without infringing on personal freedoms, and companies
-		leverage AI automation without exposing their sensitive data. At Caligo, we're not just
-		imagining this world; we're building it.
+	<HeroText background={privacy} title="AI designed for privacy">
+		Caligo allows you to leverage the benefits of modern AI technologies without compromising on the
+		confidentiality of your data. Our proprietary algorithm enables AI to process directly encrypted
+		data, without ever decrypting it. This means you can harness the power of artificial
+		intelligence while keeping your data completely secure.
 	</HeroText>
 
-	<HeroText background={padlock} title="Envision a Future Where Privacy and AI Coexist">
-		In an era where data is king, the conventional use of artificial intelligence poses a paradox;
-		it demands access to vast amounts of personal and sensitive data, risking privacy and security.
-		Businesses and individuals alike are forced to choose between innovation and privacy.
+	<HeroText background={processing} title="We can't steal your data - because we don't have it">
+		Our unique approach to AI ensures that your data is never stored or shared. Instead, our
+		algorithm processes your data in real-time, without ever decrypting it. This means that you can
+		run AI pipelines on confidential data such as medical records, legal records, financial
+		information, and more, without ever having to worry about data breaches or privacy violations.
 	</HeroText>
 
-	<HeroText background={algorithm} title="Revolutionizing AI with Encryption-in-Use Technology">
-		Caligo is pioneering a groundbreaking solution. Our proprietary algorithm enables AI to process
-		directly encrypted data, without ever decrypting it. This means you can harness the power of
-		artificial intelligence while keeping your data completely secure.
+	<HeroText background={isolated} title="Tailored to your needs">
+		Our models can be fine-tuned to meet your specific requirements, again without ever exposing
+		your training data -- not even to us. Your model runs on an isolated instance, and all training
+		data is obfuscated on your premises before being transmitted.
 	</HeroText>
 
-	<HeroText background={bird} title="Mission" id="mission">
-		Our mission is to seamlessly integrate advanced AI with ironclad privacy. We are dedicated to
-		developing advanced AI technologies that enhance public safety, drive innovation, and empower
-		businesses, all while upholding stringent privacy standards. Through our encryption-in-use
-		technology, we ensure that personal and corporate data remains secure, paving the way for a
-		world where technological progress does not come at the expense of privacy. At Caligo, we
-		believe in protecting what's valuable — your privacy, your data, and your trust.
+	<HeroText background={mandala} title="Mission" id="mission">
+		Caligo is at the forefront of integrating privacy with artificial intelligence. Our mission is
+		to provide powerful AI solutions that are inherently private and secure through our innovative
+		obfuscation technology. We ensure that sensitive data never needs to be compromised, enabling
+		businesses as well as public and private organizations to embrace AI with confidence. At Caligo,
+		we protect your privacy as we propel your progress, making AI safe and accessible for all.
 	</HeroText>
 
 	<AboutUs />
